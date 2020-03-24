8cc0fd5fbf7f6c8dda642e3f41f3fc40ce551f6d
David <davisbd100@gmail.com>
2020-03-24 15:22:08 -0600

    Creacion de ventana y contador manual

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace contador_hilos_davisbd100
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btAdd_Click(object sender, RoutedEventArgs e)
+        {
+            int.TryParse(tblManual.Text, out int result);
+            result = result+1;
+            tblManual.Text = result.ToString();
+        }
+
+        private void btStart_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
